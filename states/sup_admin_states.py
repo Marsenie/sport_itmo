e6844aef9d67cbb10157fff27c18c0bb271adcda
Marsenie <ars.rys.ry@gmail.com>
2026-02-25 23:21:17 +0300

    Add files via upload

--- states/sup_admin_states.py
+++ states/sup_admin_states.py
@@ -2,10 +2,14 @@ from aiogram.fsm.state import StatesGroup, State
 
 class SupAdmStates(StatesGroup):
     sup = State()
+
+    get_tickets = State()
     
     get = State()
     choice = State()
     msg = State()
     state = State()
+    confirm_msg = State()
     confirm = State()
+    close = State()
     
