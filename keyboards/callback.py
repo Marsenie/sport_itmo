66eb8fea67cd3dc53d2e59c355c83dd291c5253a
Marsenie <ars.rys.ry@gmail.com>
2026-02-26 20:30:11 +0300

    Add files via upload

--- keyboards/callback.py
+++ keyboards/callback.py
@@ -3,7 +3,7 @@ from aiogram.types import InlineKeyboardMarkup
 
 place_kb_list = [
     [InlineKeyboardButton(text='Ломо', callback_data='2'), InlineKeyboardButton(text='Вязьма', callback_data='3')],
-    [InlineKeyboardButton(text='онлайн', callback_data='1'), InlineKeyboardButton(text='Другие', callback_data='4')],
+    #[InlineKeyboardButton(text='онлайн', callback_data='1'), InlineKeyboardButton(text='Другие', callback_data='4')],
     [InlineKeyboardButton(text='Назад', callback_data='Назад')]
 ]
 place_kb = InlineKeyboardMarkup(inline_keyboard=place_kb_list)
